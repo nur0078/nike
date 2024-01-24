6fe560cb4a12b0b992a476646e6904980f51ee5c
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-24 21:55:47 +1100

    dynamic shoes on the shoeCard

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,10 +7,12 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
 
   return (
     <div
-      className={`border-2 rounded-xl
-    ${
-      bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
-    } cursor-pointer max-sm:flex-1
+      className={`border-2 rounded-xl 
+      ${
+        bigShoeImg === imgURL.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1
     `}
       onClick={handleCLick}
     >
