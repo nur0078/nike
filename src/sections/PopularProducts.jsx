8e44a0265e09d13e2b40672e53f0b7e0d0857087
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-25 00:20:51 +1100

    code refactor

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,27 +1,25 @@
-import ProductCard from "../components/ProductCard";
+import PopularProductCard from "../components/PopularProductCard";
 import { products } from "../constants";
 
 const PopularProducts = () => {
   return (
-    <div className="">
-      <p className="text-3xl my-4  font-bold font-montserrat">
-        Our <span className="text-coral-red"> Popular </span> Products
-      </p>
-      <p className=" text-sm text-slate-gray mb-10">
-        Experience top-noch quality and style with our sough-after selections.
-        <br />
-        Discover a world of comfort, design, and value.
-      </p>
-      <div className="flex justify-around gap-4">
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-bold font-palanquin">
+          Our
+          <span className="text-coral-red"> Popular </span> Products
+        </h2>
+        <p className=" lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Experience top-noch quality and style with our sough-after selections.
+          Discover a world of comfort, design, and value.
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gasp-14">
         {products.map((product) => (
-          <div key={product} className="">
-            <div className="">
-              <ProductCard product={product} />
-            </div>
-          </div>
+          <PopularProductCard key={product.name} {...product} />
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
