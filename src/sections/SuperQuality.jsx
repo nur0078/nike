c414541caa1f4eb03f313cd5d427a3e29ec3160d
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-25 11:36:06 +1100

    initiate adding SuperQUality

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,10 +1,27 @@
-
+import Button from "../components/Button";
 const SuperQuality = () => {
   return (
     <div>
-      SuperQuality
-    </div>
-  )
-}
+      <section
+        id="about-us"
+        className="flex justify-between items-center max-lg:flex-col gap-10 w-full macontainer"
+      >
+        <div className="flex flex-1 flex-col">
+          <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+            We Provide You
+            <span className="text-coral-red">Super</span>
+            <span className="text-coral-red">Quality</span> Shoes
+          </h2>
+          <p className="mt-4">
+            Discover stylish Nike arrivals, quality comfor, and innovation for
+            your active life.
+          </p>
 
-export default SuperQuality
+          <Button label="View details" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SuperQuality;
