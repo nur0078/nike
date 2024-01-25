fce1c773e338343c14e8e4243cfe70c3b873d91b
Anurodh Pandey <anurodhp7@gmail.com>
2024-01-26 03:21:05 +1100

    fix Dynamic shoe thumbnmail

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe colletion"
           width={610}
           height={500}
